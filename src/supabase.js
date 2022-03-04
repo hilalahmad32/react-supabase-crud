2247236a9ab4e4fcfcfa5620952a6ba77e1380e4
hilalahmad32 <hakofficial05@gmail.com>
2022-03-03 23:02:24 -0800

    change in supabase file

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlneXlwcmpqZml0a3Rna3p0YmxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDYzMjQ2MDMsImV4cCI6MTk2MTkwMDYwM30.6k_g7qXjrjE7rT0KrR2ICvOsMSVTSq7l_omcQZmjRrQ"
+const key = ""
 
-const url = "https://igyyprjjfitktgkztblk.supabase.co"
+const url = ""
 
 export const supabase = createClient(url, key)
